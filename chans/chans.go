f42f45de0ddee7e2822f5db2c0d0391f21102c36
yihubaikai <82234032@qq.com>
2022-07-03 14:23:34 +0800

    Add files via upload

---
+++ chans/chans.go
@@ -0,0 +1,73 @@
+package chans
+
+import (
+	"fmt"
+	"time"
+	"github.com/yihubaikai/gopublic/net"
+)
+
+
+//------------群处理模块----------------------------start  --------------------
+//全局变量
+
+var jobs chan string;
+var _URL string
+
+//--------------------------------------------------
+//定义两个结构体
+type Server struct {
+	Name string `json:"name"`
+	Link string `json:"link"`
+}
+
+func Sleep(){
+	time.Sleep( 1 )
+}
+
+//传入请求
+func PutString(text string) string {
+	jobs <- string(text)
+	fmt.Println("PutString:", text)
+	return text
+}
+
+//解析传入字符
+func GetString(szText string) string {
+  	s := make(map[string]string)
+	s["msg"] = szText
+	ret := hNet.Httpget(_URL, s)
+    return ret
+}
+
+//创建一个解析线程
+//这个是工作线程，处理具体的业务逻辑，将jobs中的任务取出，处理后将处理结果放置在results中。
+func worker(id int, jobs <-chan string) {
+	for j := range jobs {
+		GetString(j)
+	}
+}
+
+
+func RunWork(url string) {
+	//两个channel，一个用来放置工作项，一个用来存放处理结果。
+	jobs = make(chan string, 1000)
+	_URL = url
+	// 开启三个线程，也就是说线程池中只有3个线程，实际情况下，我们可以根据需要动态增加或减少线程。
+	for w := 0; w < 10; w++ {
+		go worker(w, jobs)
+	}
+}
+
+/*func main(){
+	var name string
+	go chans.RunWork("http://156.251.30.67:8000/telegram/")
+	time.Sleep( 1 )
+
+	for i := 0; i < 10; i++ {
+		s := fmt.Sprintf("%v", i)
+		chans.PutString(s)
+	}
+	fmt.Print  ("按任意键退出...")
+  	fmt.Scanln(&name)
+}
+*/
