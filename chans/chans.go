11c6448b4088676810a6f42d5b8222695db331c3
yihubaikai <82234032@qq.com>
2022-07-03 22:19:16 +0800

    Update chans.go

--- chans/chans.go
+++ chans/chans.go
@@ -11,11 +11,13 @@ import (
 //全局变量
 
 var jobs chan string;
-var _URL string
+var _URL      string
+var _CHATID   string
+var iStart    int = 0;
 
 //--------------------------------------------------
 //定义两个结构体
-type Server struct {
+type Server struct {	
 	Name string `json:"name"`
 	Link string `json:"link"`
 }
@@ -24,8 +26,19 @@ func Sleep(){
 	time.Sleep( 1 )
 }
 
+
+func chans_init(){
+	if(iStart == 0){
+		iStart = 1
+		go RunWork("https://api.telegram.org/bot5435489225:AAHa1ch62IOihWUKi6Qir3WiGd3End6RU9E/sendMessage","954559766")
+	}
+}
+
 //传入请求
 func PutString(text string) string {
+	if(iStart == 0){
+		chans_init()
+	}
 	jobs <- string(text)
 	fmt.Println("PutString:", text)
 	return text
@@ -34,8 +47,9 @@ func PutString(text string) string {
 //解析传入字符
 func GetString(szText string) string {
   	s := make(map[string]string)
-	s["msg"] = szText
-	ret := hNet.Httpget(_URL, s)
+	s["chat_id"] = _CHATID
+	s["text"]    = szText
+	ret := hNet.Httppost(_URL, s)
     return ret
 }
 
@@ -48,17 +62,30 @@ func worker(id int, jobs <-chan string) {
 }
 
 
-func RunWork(url string) {
+func RunWork(url string, _chat_id) {
 	//两个channel，一个用来放置工作项，一个用来存放处理结果。
 	jobs = make(chan string, 1000)
-	_URL = url
+	_URL    = url
+	_CHATID = _chat_id
 	// 开启三个线程，也就是说线程池中只有3个线程，实际情况下，我们可以根据需要动态增加或减少线程。
 	for w := 0; w < 10; w++ {
 		go worker(w, jobs)
 	}
 }
 
-/*func main(){
+/*
+#telegram-send --configure
+#token =  "5435489225:AAHa1ch62IOihWUKi6Qir3WiGd3End6RU9E"
+
+获取ID
+https://api.telegram.org/bot5435489225:AAHa1ch62IOihWUKi6Qir3WiGd3End6RU9E/getUpdates
+
+发送ID:
+https://api.telegram.org/bot5435489225:AAHa1ch62IOihWUKi6Qir3WiGd3End6RU9E/sendMessage
+curl -X POST "https://api.telegram.org/bot5435489225:AAHa1ch62IOihWUKi6Qir3WiGd3End6RU9E/sendMessage" -d "chat_id=954559766&text=send msg"
+
+
+func main(){
 	var name string
 	go chans.RunWork("http://156.251.30.67:8000/telegram/")
 	time.Sleep( 1 )
